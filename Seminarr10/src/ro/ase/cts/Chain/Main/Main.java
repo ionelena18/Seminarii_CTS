6c9c4ee1d75daeeb7b16de55bd3d7a8a6b844eae
Cristina <ionelenacristina0@gmail.com>
2021-05-08 22:41:04 +0300

    Chain_Of_Responsability

---
+++ Seminarr10/src/ro/ase/cts/Chain/Main/Main.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.Chain.Main;
+
+import ro.ase.cts.Chain.Clase.Client;
+import ro.ase.cts.Chain.Clase.NotificatorEmail;
+import ro.ase.cts.Chain.Clase.NotificatorSMS;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client1=new Client("Ion",null,null);
+        Client client2=new Client("Maria","072257893",null);
+        Client client3=new Client("Ana",null,"ana2@gmail.com");
+
+        NotificatorSMS notificatorSMS=new NotificatorSMS();
+        NotificatorEmail notificatorEmail=new NotificatorEmail();
+        notificatorSMS.setNotificator(notificatorEmail);
+        notificatorSMS.trimiteNotificare(client2,"Mesaj1");
+    }
+}
