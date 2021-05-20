19ffd56a72d8ccd6f1dd72ee71d65c529e961f41
Cristina <ionelenacristina0@gmail.com>
2021-05-20 20:38:12 +0300

    Test

--- Seminar12/src/ro/ase/cts/tests/StudentTest.java
+++ Seminar12/src/ro/ase/cts/tests/StudentTest.java
@@ -7,13 +7,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
+
 public class StudentTest {
     @Test
-    public void testConstructorCuUnParametru(){
+    public void testConstructorCuUnParametru() {
         //verificam daca un student a fost construit prin nume
-        String nume="Gigel";
-        Student student=new Student(nume);
-        assertEquals(nume,student.getNume());
+        String nume = "Gigel";
+        Student student = new Student(nume);
+        assertEquals(nume, student.getNume());
 
         //verificam daca are note
         assertNotNull(student.getNote());
@@ -22,63 +23,101 @@ public class StudentTest {
     }
 
     @Test
-    public void testConstructorFaraParametru(){
-        Student student=new Student();
-        assertNotNull(student.getNote());
-        assertNotNull(student.getNume());
+    public void testConstructorFaraParametru() {
+        Student student = new Student();
+        assertNotNull("Atributul note nu a fost initializat", student.getNote());
+        assertNotNull("Atributul nume nu a fost initializat", student.getNume());
     }
 
     @Test
-    public void testAdaugaNota(){
-        Student student=new Student();
-        int nota=5;
+    public void testAdaugaNota() {
+        Student student = new Student();
+        int nota = 5;
         student.adaugaNota(nota);
-        assertEquals(nota,student.getNota(0));
+        assertEquals(nota, student.getNota(0));
 
     }
 
     @Test
-    public void testDimensiuneNote(){
-        Student student=new Student();
+    public void testDimensiuneNote() {
+        Student student = new Student();
         student.adaugaNota(5);
         student.adaugaNota(8);
-        assertEquals(2,student.getNote().size());
+        assertEquals(2, student.getNote().size());
     }
 
     @Test
-    public void testCalculeazaMedie(){
-        Student student=new Student();
+    public void testCalculeazaMedie() {
+        Student student = new Student();
         student.adaugaNota(5);
         student.adaugaNota(8);
-        assertEquals(6.5F,student.calculeazaMedie(),0.1);
+        assertEquals(6.5F, student.calculeazaMedie(), 0.1);
     }
 
     @Test
-    public void testCalculeazaMedieFaraNote(){
-        Student student=new Student();
-        assertEquals(0F,student.calculeazaMedie(),0.1);
+    public void testCalculeazaMedieFaraNote() {
+        Student student = new Student();
+        assertEquals(0F, student.calculeazaMedie(), 0.1);
     }
 
     @Test
-    public void testCalculeazaMedieCuONota(){
-        Student student=new Student();
+    public void testCalculeazaMedieCuONota() {
+        Student student = new Student();
         student.adaugaNota(5);
-        assertEquals(5,student.calculeazaMedie(),0.1);
+        assertEquals(5, student.calculeazaMedie(), 0.1);
     }
 
     @Test
-    public void testAreRestante(){
-        Student student=new Student();
+    public void testAreRestante() {
+        Student student = new Student();
         student.adaugaNota(9);
         student.adaugaNota(3);
         assertTrue(student.areRestante());
     }
 
     @Test
-    public void testNuAreRestante(){
-        Student student=new Student();
+    public void testNuAreRestante() {
+        Student student = new Student();
         student.adaugaNota(9);
         student.adaugaNota(10);
         assertFalse(student.areRestante());
     }
+
+    @Test
+    public void testGetNotaExceptie() {
+        Student student = new Student();
+        student.adaugaNota(1);
+        student.adaugaNota(10);
+        try {
+            student.getNota(5);
+            fail("Nu arunca exceptie");
+        } catch (IndexOutOfBoundsException e) {
+
+        }
+        catch (Exception e){
+            fail("Arunca o alta exceptie");
+        }
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNotaExceptieJUnit4() {
+        Student student = new Student();
+        student.adaugaNota(1);
+        student.adaugaNota(10);
+        student.getNota(5);
+
+    }
+
+    @Test
+    public void testGetNotaExceptieJUnit5() {
+        Student student = new Student();
+        student.adaugaNota(1);
+        student.adaugaNota(10);
+        student.getNota(5);
+        //assertThrows(IndexOutOfBoundsException.class,()->{
+        //  student.getNota(5);
+        // }
+        // )
+    }
+
 }
